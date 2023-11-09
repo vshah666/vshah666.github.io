6fe2559b6f959542115471af5cfe2c2b81a183ee
vshah666 <75186407+vshah666@users.noreply.github.com>
2023-11-09 03:27:57 -0500

    Added homepage

---
+++ web.js
@@ -0,0 +1,9 @@
+document.getElementById("notupyet").onclick = myFunction;
+document.getElementById("notupyet1").onclick = myFunction;
+document.getElementById("notupyet2").onclick = myFunction;
+document.getElementById("notupyet3").onclick = myFunction;
+document.getElementById("notupyet4").onclick = myFunction;
+document.getElementById("notupyet5").onclick = myFunction;
+function myFunction(){
+    alert("This page will be up shortly, please check back later!");
+}
